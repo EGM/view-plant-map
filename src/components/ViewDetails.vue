e02e081d60dff5aa2d3d2db70ae77748ae7cfecf
John LaDuke <JLaDuke@ci.palm-coast.fl.us>
2025-03-06 09:34:27 -0500

    Styling changes.

--- src/components/ViewDetails.vue
+++ src/components/ViewDetails.vue
@@ -4,13 +4,22 @@ defineProps<{ feature: any }>()
 
 <template>
     <div>
-        ID: {{ feature.id }}<br />
-        Name: {{ feature.properties.name }}
+        <p><label>ID:</label> {{ feature.id }}</p>
+        <p><label>Name:</label> {{ feature.properties.name }}</p>
+        <p><label>Description:</label> {{ feature.properties.description }}</p>
     </div>
 </template>
 
 <style scoped>
     div {
         padding-left: 1rem;
+        font-size: 1.125rem;
+    }
+    label {
+        color: #300;
+        font-size: 0.875rem;
+    }
+    p {
+        margin-top: 0.5rem;
     }
 </style>
